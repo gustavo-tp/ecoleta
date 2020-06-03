3dba128015ca5e98a01b3bd00dcde4e3dc4151b1
gustavo-tp <gustavo1190.gt@gmail.com>
2020-06-03 00:59:54 -0300

    Back-end da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
-import express, { response } from 'express';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  response.json(['Diego', 'Mayk', 'Deschamps', 'Teló']);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
